99f58abf68d2ae8599b97e3be275ecefbfcfa4ad
Vineet Singh Parihar <2023uee2016@mnit.ac.in>
2026-06-17 12:57:46 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.js",
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  devServer: {
+    static: "./dist",
+    open: true,
+    hot: false,
+    liveReload: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
