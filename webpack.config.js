c4437d05648df291e0e54c9d4e53e98db4d753f6
MuscleQuant <2023uee2016@mnit.ac.in>
2026-06-18 19:06:21 +0530

    completed with gifs,weirdness=10

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const env = dotenv.config().parsed;
 
+
 export default {
   entry: "./src/index.js",
 
